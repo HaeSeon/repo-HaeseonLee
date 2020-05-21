481e7b0377f11b58b77348b21b4f4681102f73e9
HaeSeon <gildogi@naver.com>
2020-05-22 01:32:16 +0900

    commit

--- music-mixing-v1/app/src/main/java/com/example/music_mixing_v1/MainActivity.kt
+++ music-mixing-v1/app/src/main/java/com/example/music_mixing_v1/MainActivity.kt
@@ -20,15 +20,15 @@ class MainActivity : AppCompatActivity() {
         //문제2. BPM, 분위기 등 어울리는 샘플 조합을 인공지능을 쓸 수 없기 때문에 우리가 직접 다 골라내고 맞춰야함
         //문제3. 감정, 날씨, 시간, 계절, 위치 분류에서 조합되는 카테고리가 너무 많음 -> 그 음원 파일을 다 직접 다듬기가 사실상 불가능
         //문제4. 그 분류에 맞춰서 다듬고 분류를 한다해도 사용자가 완성음원을 들었을 때 겹치는 음악이 상당수 나올것으로 예상됨
-        //문제5. 코틀린 내장 음원플레이인 미디어플레이어 사용시 음질이 심히 꺠진다.
+        //문제5. 코틀린 내장 음원플레이인 미디어플레이어 사용시 음질이 심히 깨진다.
 
         mediaplayer1 = MediaPlayer.create(this, R.raw.drum)
         mediaplayer2 = MediaPlayer.create(this, R.raw.piano)
 
 
-        //버튼을 클릭하면 mp3를 재생하도록 하자.
+        //버튼을 클릭하면 음원 재생
         buttonPlay.setOnClickListener {
-            //재생할 파일을 준비한다.
+            //재생할 파일을 준비
             mediaplayer1?.start()
             mediaplayer2?.start()
         }
